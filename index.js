b51ec362363588a3ad202b35a091d9926994ac91
llee-125 <llee@berkeley.edu>
2020-07-01 13:08:01 -0700

    Updated SQL for ViewEmployeesByDept. Doesn't work correctly, need instruction on how to select departments.

--- index.js
+++ index.js
@@ -103,8 +103,13 @@ function searchEmployees() {
 }
 
 function viewAllEmployees() {
-  var query =
-    "SELECT employee.id, employee.fname, employee.lname, role.title, department.name AS department, role.salary, CONCAT(manager.fname, ' ', manager.lname) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.dept_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id";
+  var query = `SELECT employee.id, employee.fname, employee.lname, role.title, department.name AS department, role.salary, 
+    CONCAT(manager.fname, ' ', manager.lname) AS manager 
+    FROM employee 
+    LEFT JOIN role on employee.role_id = role.id 
+    LEFT JOIN department on role.dept_id = department.id 
+    LEFT JOIN employee manager on manager.id = employee.manager_id`;
+
   connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
@@ -116,106 +121,109 @@ function viewEmployeesByDepartment() {
   inquirer
     .prompt({
       name: "employeesByDepartment",
-      type: "rawlist",
+      type: "input",
       message: "What department would you like to view?",
       choices: ["Sales", "Marketing", "Accounts Payable", "IT", "exit"],
     })
     .then(function (a) {
-      var query =
-        "SELECT employee.id, employee.fname, employee.lname, role.title, department.name AS department, role.salary, CONCAT(manager.fname, ' ', manager.lname)";
-      connection.query(query, function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        searchEmployees();
+      return new Promise((resolve, reject) => {
+        connection.query(
+          "SELECT * FROM employee LEFT JOIN role on employee.role_id=role.id WHERE ?",
+          [{ text: userText }],
+          (err, data) => {
+            err ? reject(err) : resolve(data);
+            console.table(res);
+          }
+        );
       });
     });
-}
 
-function addEmployee(userText) {
-  return new Promise((res, rej) => {
-    connection.query(
-      "INSERT INTO employee SET ?",
-      [{ text: userText }],
-      (err) => {
-        err ? rej(err) : res({ msg: "success" });
-        console.table(res);
-      }
-    );
-  });
-}
-
-function removeEmployee() {
-  inquirer
-    .prompt({
-      name: "list",
-      type: "input",
-      message: "Which employee would you like to remove?",
-    })
-    .then(function (answer) {
-      console.log(answer.song);
+  function addEmployee(userText) {
+    return new Promise((res, rej) => {
       connection.query(
-        "SELECT * FROM employee.fname WHERE ?",
-        { song: answer.song },
-        function (err, res) {
-          console.log(
-            "Position: " +
-              res[0].position +
-              " || Song: " +
-              res[0].song +
-              " || Artist: " +
-              res[0].artist +
-              " || Year: " +
-              res[0].year
-          );
-          searchEmployees();
+        "INSERT INTO employee SET ?",
+        [{ text: userText }],
+        (err) => {
+          err ? rej(err) : res({ msg: "success" });
+          console.table(res);
         }
       );
     });
-}
+  }
 
-function songAndAlbumSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?",
-    })
-    .then(function (answer) {
-      var query =
-        "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-      query +=
-        "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-      query +=
-        "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-      connection.query(query, [answer.artist, answer.artist], function (
-        err,
-        res
-      ) {
-        console.log(res.length + " matches found!");
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            i +
-              1 +
-              ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
-              " || Album: " +
-              res[i].album
-          );
-        }
-
-        searchEmployees();
+  function removeEmployee() {
+    inquirer
+      .prompt({
+        name: "list",
+        type: "input",
+        message: "Which employee would you like to remove?",
+      })
+      .then(function (answer) {
+        console.log(answer.song);
+        connection.query(
+          "SELECT * FROM employee.fname WHERE ?",
+          { song: answer.song },
+          function (err, res) {
+            console.log(
+              "Position: " +
+                res[0].position +
+                " || Song: " +
+                res[0].song +
+                " || Artist: " +
+                res[0].artist +
+                " || Year: " +
+                res[0].year
+            );
+            searchEmployees();
+          }
+        );
       });
-    });
-}
+  }
 
-const quit = () => {
-  process.exit();
-};
+  function songAndAlbumSearch() {
+    inquirer
+      .prompt({
+        name: "artist",
+        type: "input",
+        message: "What artist would you like to search for?",
+      })
+      .then(function (answer) {
+        var query =
+          "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
+        query +=
+          "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
+        query +=
+          "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+
+        connection.query(query, [answer.artist, answer.artist], function (
+          err,
+          res
+        ) {
+          console.log(res.length + " matches found!");
+          for (var i = 0; i < res.length; i++) {
+            console.log(
+              i +
+                1 +
+                ".) " +
+                "Year: " +
+                res[i].year +
+                " Album Position: " +
+                res[i].position +
+                " || Artist: " +
+                res[i].artist +
+                " || Song: " +
+                res[i].song +
+                " || Album: " +
+                res[i].album
+            );
+          }
+
+          searchEmployees();
+        });
+      });
+  }
+
+  const quit = () => {
+    process.exit();
+  };
+}
